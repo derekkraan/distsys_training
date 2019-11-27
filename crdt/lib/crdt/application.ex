e007016e549e9a403483f95da3d3a160f4f40b05
Derek Kraan <derek.kraan@gmail.com>
2019-11-27 12:57:21 +0100

    Add application to CRDT so we can test with LocalCluster later

---
+++ crdt/lib/crdt/application.ex
@@ -0,0 +1,19 @@
+defmodule Crdt.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Crdt.Worker.start_link(arg)
+      # {Crdt.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Crdt.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
